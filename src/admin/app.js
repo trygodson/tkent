a72405f3028971e2ba4a791b54e63ba4db68180d
Godson <gnqnpn@gmail.com>
2024-06-02 21:53:16 +0100

    some changes

---
+++ src/admin/app.js
@@ -0,0 +1,33 @@
+import logo from "./extentions/icon.png";
+
+const config = {
+  auth: {
+    logo,
+  },
+  head: {
+    favicon: logo,
+  },
+  menu: {
+    logo,
+  },
+  translations: {
+    en: {
+      "app.components.LeftMenu.navbrand.title": "Tk Enterprise",
+      "Auth.form.welcome.title": "Welcome to Tk Enterprise",
+      "Auth.form.welcome.subtitle": "Login To Yur Account",
+    },
+  },
+  tutorials: false,
+  notifications: {
+    release: false,
+  },
+};
+
+const bootstrap = (app) => {
+  console.log(app);
+};
+
+export default {
+  config,
+  bootstrap,
+};
