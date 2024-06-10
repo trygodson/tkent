9ab1e69a9ee26b2a6b93542e7dfe79dce8823988
Godson <gnqnpn@gmail.com>
2024-06-10 11:09:45 +0100

    some changs

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -46,6 +46,11 @@ module.exports = createCoreController("api::order.order", ({strapi}) => {
             };
           })
         );
+
+        console.log(
+          orderList.map((d) => d.productId),
+          "--products ids---"
+        );
         const theOrders = await strapi.service("api::order.order").create({
           data: {
             products: orderList.map((d) => d.productId),
